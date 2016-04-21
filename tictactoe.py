ddec1780f423c4da5895a97268e3d65c2df0dcae
Lisa Watkins <Lisa Watkins>
2016-04-21 13:09:38 -0500

    game progress 4/21

---
+++ tictactoe.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+
+from IPython.display import clear_output
+def display_board(bprint('   |   |'):
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+print "Player 1, would you like to be X's or O'x?"
+
+def player_input():
+
+
+
+
+
+
