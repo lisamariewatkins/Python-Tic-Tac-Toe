327a91b7a740b39e9c4054d6eca52fe57abb899f
Lisa Watkins <Lisa Watkins>
2016-07-06 10:15:29 -0500

    game works!

--- tictactoe.py
+++ tictactoe.py
@@ -10,8 +10,8 @@ def displayBoard(board):
 
 def inputLetter(): #lets player type in what letter they'd like to be
     letter = ''
-    while not letter == 'X' or letter == 'O':
-        letter = raw_input("Do you want to be X or O?").upper()
+    while not (letter == 'X' or letter == 'O'):
+        letter = raw_input("Player 1, do you want to be X or O?").upper()
     if letter == "X":
         return ("X", "O")
     else:
@@ -26,8 +26,8 @@ def first():
 def playAgain():
     return raw_input("Do you want to play again?").lower().startswith("y")
 
-def move(board, letter, move):
-    board[move] = letter
+def move(board, letter, position):
+    board[position] = letter
 
 def whoWon(board, letter):
     return (board[0] == board[1] == board [2] == letter or 
@@ -40,20 +40,22 @@ def whoWon(board, letter):
        board[2] == board[4] == board [6] == letter) 
 
 def spaceCheck(board, position):
-    return board[position] == ""
+    return board[position] == " "
 
 def fullBoard(board):
     for i in range(1,10):
-        if space_check(board,i):
+        if spaceCheck(board,i):
             return False
     return True
 
 def chooseSpace(board):
-    position = ''
-
+    position = ' '
     while position not in '1 2 3 4 5 6 7 8 9'.split() or not spaceCheck(board,int(position)):
         position = raw_input("Choose your next position: (1-9)")
     return int(position)
+
+def replay():
+	return raw_input("Do you want to play again? Enter Yes or No: ").lower().startswith('y')
 
 print("Welcome to Tic-Tac-Toe")
 
@@ -62,7 +64,6 @@ while True:
     player1Marker, player2Marker = inputLetter()
     turn = first()
     print(turn + " goes first")
-
     gameOn = True
 
     while gameOn:
@@ -71,12 +72,12 @@ while True:
             position = chooseSpace(board)
             move(board, player1Marker, position)
 
-            if winCheck(board, player1_marker):
+            if whoWon(board, player1Marker):
                 displayBoard(board)
                 print("Congratulations player 1")
                 gameOn = False
             else:
-                if full_board_check(theBOard):
+                if fullBoard(board):
                     displayBoard(board)
                     print("the game is a draw")
                     break
@@ -87,12 +88,12 @@ while True:
             position = chooseSpace(board)
             move(board, player2Marker, position)
 
-            if winCheck(board, player2Marker):
+            if whoWon(board, player2Marker):
                 displayBoard(board)
                 print("Congratulations player 2")
                 gameOn = False
             else:
-                if fullboard(board):
+                if fullBoard(board):
                     displayBoard(board)
                     print("the game is a draw")
                     break
